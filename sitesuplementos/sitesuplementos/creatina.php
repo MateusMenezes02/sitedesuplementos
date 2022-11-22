ccf4a71197472b023b308dbcaefc4d39a7138ae1
Mateus <104095851+MateusMenezes02@users.noreply.github.com>
2022-11-22 19:05:29 -0300

    os arquivos do site

---
+++ sitesuplementos/sitesuplementos/creatina.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>StrongSuplementos</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="creatina.css">
+  </head>
+  <body class="p-3 m-0 border-0 bd-example">
+
+    <!-- Example Code -->
+    
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Strong Suplementos</font></font></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Suplementos</font></font></a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="creatina.php">Creatina</a></li>
+          <li><a class="dropdown-item" href="whey.php">Whey</a></li>
+          <li><a class="dropdown-item" href="bcaa.php">BCAA</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="home.php">HOME</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="quemsomos.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quem Somos?</font></font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Admin</font></font></a>
+      </li>
+    </ul>
+    
+    <!-- End Example Code -->
+   
+ <img src="imagens/creatina_resized.png" alt="Creatina">
+
+ <p class="info">Qual a função da creatina no corpo?<br>
+Ela apresenta a função de fornecer energia e desenvolver fibras musculares, com o benefício de ganhar massa muscular e melhorar o desempenho físico. Mesmo sendo uma substância produzida naturalmente pelo organismo, ainda é comum que atletas façam uso da creatina para auxiliar na performance de forma geral.</p>
+
+    
---
+++ sitesuplementos/sitesuplementos/whey.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>StrongSuplementos</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="creatina.css">
+  </head>
+  <body class="p-3 m-0 border-0 bd-example">
+
+    <!-- Example Code -->
+    
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="home.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Strong Suplementos</font></font></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Suplementos</font></font></a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="creatina.php">Creatina</a></li>
+          <li><a class="dropdown-item" href="whey.php">Whey</a></li>
+          <li><a class="dropdown-item" href="bcaa.php">BCAA</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="home.php">HOME</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="quemsomos.php"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Quem Somos?</font></font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Admin</font></font></a>
+      </li>
+    </ul>
+    
+    <!-- End Example Code -->
+   
+ <img src="imagens/whey_resized.png" alt="whey">
+
+ <p class="info">Para que serve e quais são os benefícios do whey protein?<br> Além das proteínas e dos aminoácidos, o whey protein tem alta concentração de cálcio, trazendo benefícios para a saúde óssea do corpo. O consumo do suplemento pode ajudar no aumento da força e da massa muscular após a prática de atividades intensas </p>
+    
